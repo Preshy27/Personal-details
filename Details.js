321cd3dcd5d21aad0b1001b0fbf54a1ff08f7f75
Precious Aienobe <105337990+Preshy27@users.noreply.github.com>
2022-06-22 08:58:08 +0100

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,7 @@
+let name = "My name is Precious Aienobe" + "<br>";
+let height = "My height is 1.81m" + "<br>";
+let country = "My country is Nigeria";
+
+document.write(name);
+document.write(height);
+document.write(country);
